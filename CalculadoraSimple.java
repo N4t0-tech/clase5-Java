92e03bc5f4dbea9d32bc2979de787c08f6e27c89
Renato Campos <renaxd13@gmail.com>
2026-04-07 14:33:02 -0400

    feat:aparte el menu hacia una funcion fuera del main

--- CalculadoraSimple.java
+++ CalculadoraSimple.java
@@ -3,98 +3,101 @@ import java.lang.Math;
 public class CalculadoraSimple {
 
     public static void main(String[] args) {
-        boolean condition = true;
         Scanner scanner = new Scanner(System.in);
+        boolean continuar;
         do {
-
-            System.out.println("                     /$$                     /$$             /$$                        ");
-            System.out.println("                    | $$                    | $$            | $$                        ");
-            System.out.println("  /$$$$$$$  /$$$$$$ | $$  /$$$$$$$ /$$   /$$| $$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$  ");
-            System.out.println(" /$$_____/ |____  $$| $$ /$$_____/| $$  | $$| $$ |____  $$|_  $$_/   /$$__  $$ /$$__  $$");
-            System.out.println("| $$        /$$$$$$$| $$| $$      | $$  | $$| $$  /$$$$$$$  | $$    | $$  \\ $$| $$  \\__/");
-            System.out.println("| $$       /$$__  $$| $$| $$      | $$  | $$| $$ /$$__  $$  | $$ /$$| $$  | $$| $$      ");
-            System.out.println("|  $$$$$$$|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$|  $$$$$$$  |  $$$$/|  $$$$$$/| $$      ");
-            System.out.println(" \\_______/ \\_______/|__/ \\_______/ \\______/ |__/ \\_______/   \\___/   \\______/ |__/");
-            System.out.println("-------------------------------Seleccione una operación:--------------------------------");
-            System.out.println("1. Suma");
-            System.out.println("2. Resta");
-            System.out.println("3. Multiplicación");
-            System.out.println("4. División");
-            System.out.println("5. Raíz cuadrada");
-            System.out.println("6. Potencia");
-            System.out.println("7. Modulo");
-            System.out.println("8. Salir");
-
-            int a, b;
+            mostrarMenu();
             int opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("Ha seleccionado suma");
-                    System.out.print("Ingrese el primer número: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el segundo número: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + suma(a, b));
-                    break;
-                case 2:
-                    System.out.println("Ha seleccionado resta");
-                    System.out.print("Ingrese el primer número: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el segundo número: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + resta(a, b));
-                    break;
-                case 3:
-                    System.out.println("Ha seleccionado multiplicación");
-                    System.out.print("Ingrese el primer número: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el segundo número: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + multiplicacion(a, b));
-                    break;
-                case 4:
-                    System.out.println("Ha seleccionado división");
-                    System.out.print("Ingrese el primer número: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el segundo número: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + division(a, b));
-                    break;
-                case 5:
-                    System.out.println("Ha seleccionado raíz cuadrada");
-                    System.out.print("Ingrese el número: ");
-                    a = scanner.nextInt();
-                    System.out.println("Resultado: " + raizCuadrada(a));
-                    break;
-                case 6:
-                    System.out.println("Ha seleccionado potencia");
-                    System.out.print("Ingrese la base: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el exponente: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + potencia(a, b));
-                    break;
-                case 7:
-                    System.out.println("Ha seleccionado módulo");
-                    System.out.print("Ingrese el primer número: ");
-                    a = scanner.nextInt();
-                    System.out.print("Ingrese el segundo número: ");
-                    b = scanner.nextInt();
-                    System.out.println("Resultado: " + modulo(a, b));
-                    break;
-
-                case 8:
-                    System.out.println("Saliendo...");
-                    condition = false;
-                    break;
-                default:
-                    System.out.println("Opción no válida");
-                    break;
-            }
-        }       
-        while(condition);
+            continuar = ejecutarOperacion(opcion, scanner);
+        } while (continuar);
         scanner.close();
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("                     /$$                     /$$             /$$                        ");
+        System.out.println("                    | $$                    | $$            | $$                        ");
+        System.out.println("  /$$$$$$$  /$$$$$$ | $$  /$$$$$$$ /$$   /$$| $$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$  ");
+        System.out.println(" /$$_____/ |____  $$| $$ /$$_____/| $$  | $$| $$ |____  $$|_  $$_/   /$$__  $$ /$$__  $$");
+        System.out.println("| $$        /$$$$$$$| $$| $$      | $$  | $$| $$  /$$$$$$$  | $$    | $$  \\ $$| $$  \\__/");
+        System.out.println("| $$       /$$__  $$| $$| $$      | $$  | $$| $$ /$$__  $$  | $$ /$$| $$  | $$| $$      ");
+        System.out.println("|  $$$$$$$|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$|  $$$$$$$  |  $$$$/|  $$$$$$/| $$      ");
+        System.out.println(" \\_______/ \\_______/|__/ \\_______/ \\______/ |__/ \\_______/   \\___/   \\______/ |__/");
+        System.out.println("-------------------------------Seleccione una operación:--------------------------------");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+        System.out.println("5. Raíz cuadrada");
+        System.out.println("6. Potencia");
+        System.out.println("7. Modulo");
+        System.out.println("8. Salir");
+    }
+
+    public static boolean ejecutarOperacion(int opcion, Scanner scanner) {
+        int a, b;
+        switch (opcion) {
+            case 1:
+                System.out.println("Ha seleccionado suma");
+                System.out.print("Ingrese el primer número: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el segundo número: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + suma(a, b));
+                break;
+            case 2:
+                System.out.println("Ha seleccionado resta");
+                System.out.print("Ingrese el primer número: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el segundo número: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + resta(a, b));
+                break;
+            case 3:
+                System.out.println("Ha seleccionado multiplicación");
+                System.out.print("Ingrese el primer número: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el segundo número: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + multiplicacion(a, b));
+                break;
+            case 4:
+                System.out.println("Ha seleccionado división");
+                System.out.print("Ingrese el primer número: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el segundo número: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + division(a, b));
+                break;
+            case 5:
+                System.out.println("Ha seleccionado raíz cuadrada");
+                System.out.print("Ingrese el número: ");
+                a = scanner.nextInt();
+                System.out.println("Resultado: " + raizCuadrada(a));
+                break;
+            case 6:
+                System.out.println("Ha seleccionado potencia");
+                System.out.print("Ingrese la base: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el exponente: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + potencia(a, b));
+                break;
+            case 7:
+                System.out.println("Ha seleccionado módulo");
+                System.out.print("Ingrese el primer número: ");
+                a = scanner.nextInt();
+                System.out.print("Ingrese el segundo número: ");
+                b = scanner.nextInt();
+                System.out.println("Resultado: " + modulo(a, b));
+                break;
+            case 8:
+                System.out.println("Saliendo...");
+                return false;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
+        return true;
     }
     
     public static int suma(int a, int b) {
